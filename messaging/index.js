b8e13108eab30b065bda8050400f688b253bfcee
Satya <satyagvns@outlook.com>
2023-02-15 13:59:33 +0530

    Fixed minor bug

--- messaging/index.js
+++ messaging/index.js
@@ -9,7 +9,7 @@ let server = http.Server(app);
 
 // Initializing socket service
 let socketIO = require('socket.io');
-let io = socketIO.Server(server);
+let io = socketIO(server);
 
 // Initializing port
 const port = process.env.PORT || 3000;
